bc1c46947076028b8d31761b694f14f0587422ba
Tari Yekorogha <trogaclassicman@gmail.com>
2022-06-07 13:56:43 +0100

    Update learning4.py

--- learning4.py
+++ learning4.py
@@ -1,2 +1,30 @@
 # Import PyDub main class
 from pydub import AudioSegment
+
+# Import an audio file
+# Format parameter only for readability
+wav_file = AudioSegment.from_file(file="good-morning-japanense.wav", format="wav")
+
+
+# Playing an audio file
+from pydub.playback import play
+#play()
+
+#Audio parameters
+wav_file.channels
+
+
+
+wav_file.frame_rate
+
+#find the number of bytes per sample
+wav_file.sample_width
+
+# Find the max amplitude
+wav_file.max
+
+#Duration of audio file in mills
+len(wav_file)
+
+#changing audio parameters
+# the higher the values excluding channels the better.
